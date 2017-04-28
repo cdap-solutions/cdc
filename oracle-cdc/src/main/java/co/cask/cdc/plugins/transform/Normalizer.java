b4a79903bf025829c8fce433eef9a7874e8f581a
sagarkapare <sagar@cask.co>
2017-04-28 13:53:51 -0700

    Updated the Normalizer to use the avro format.

--- oracle-cdc/src/main/java/co/cask/cdc/plugins/transform/Normalizer.java
+++ oracle-cdc/src/main/java/co/cask/cdc/plugins/transform/Normalizer.java
@@ -19,21 +19,28 @@ package co.cask.cdc.plugins.transform;
 import co.cask.cdap.api.annotation.Description;
 import co.cask.cdap.api.annotation.Name;
 import co.cask.cdap.api.annotation.Plugin;
-import co.cask.cdap.api.common.Bytes;
 import co.cask.cdap.api.data.format.StructuredRecord;
 import co.cask.cdap.api.data.schema.Schema;
 import co.cask.cdap.api.plugin.PluginConfig;
 import co.cask.cdap.etl.api.Emitter;
 import co.cask.cdap.etl.api.Transform;
+import co.cask.cdap.etl.api.TransformContext;
+import co.cask.cdc.common.AvroConverter;
+import com.google.common.collect.Table;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.apache.avro.SchemaNormalization;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DecoderFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  *
@@ -43,101 +50,137 @@ import java.util.Map;
 public class Normalizer extends Transform<StructuredRecord, StructuredRecord> {
   private static Logger LOG = LoggerFactory.getLogger(Normalizer.class);
   private static Gson GSON = new Gson();
-
-  private static final Schema COLUMN_SCHEMA = Schema.recordOf("Column",
-                                                              Schema.Field.of("name", Schema.of(Schema.Type.STRING)),
-                                                              Schema.Field.of("type", Schema.of(Schema.Type.STRING)),
-                                                              Schema.Field.of("value", Schema.of(Schema.Type.STRING)));
-
-
-  private static final Schema CDC_SCHEMA = Schema.recordOf("CDC",
-                                                           Schema.Field.of("table", Schema.of(Schema.Type.STRING)),
-                                                           Schema.Field.of("op_type", Schema.of(Schema.Type.STRING)),
-                                                           Schema.Field.of("op_ts", Schema.of(Schema.Type.STRING)),
-                                                           Schema.Field.of("current_ts", Schema.of(Schema.Type.STRING)),
-                                                           Schema.Field.of("before", Schema.unionOf(Schema.mapOf(Schema.of(Schema.Type.STRING), Schema.of(Schema.Type.STRING)), Schema.of(Schema.Type.NULL))),
-                                                           Schema.Field.of("after", Schema.unionOf(Schema.mapOf(Schema.of(Schema.Type.STRING), Schema.of(Schema.Type.STRING)), Schema.of(Schema.Type.NULL))));
-
-  // Schema.Field.of("before", Schema.unionOf(Schema.arrayOf(COLUMN_SCHEMA),
-  //                                         Schema.of(Schema.Type.NULL))),
-  // Schema.Field.of("after", Schema.unionOf(Schema.arrayOf(COLUMN_SCHEMA),
-  //                                        Schema.of(Schema.Type.NULL))));
+  private static final Schema CDC_DDL_SCHEMA = Schema.recordOf("DDLRecord",
+                                                               Schema.Field.of("schema", Schema.of(Schema.Type.STRING)));
 
   private final NormalizerConfig config;
+
+  private URL schemaRegistryURL;
+
+  private Table<String, Long, org.apache.avro.Schema> schemaCache;
 
   public Normalizer(NormalizerConfig config) {
     this.config = config;
   }
 
   @Override
+  public void initialize(TransformContext context) throws Exception {
+    super.initialize(context);
+    // TODO Get the schema from service
+    /*
+      String[] serviceId = config.schemaServiceName.split(":");
+      schemaRegistryURL = context.getServiceURL(serviceId[0], serviceId[1]);
+      schemaCache = HashBasedTable.create();
+    */
+  }
+
+  @Override
   public void transform(StructuredRecord input, Emitter<StructuredRecord> emitter) throws Exception {
     LOG.info("Input StructuredRecord is {}", GSON.toJson(input));
-    byte[] message = input.get("message");
+
+    byte[] message = input.get(config.getInputField());
     if (message == null) {
-      LOG.info("Message field does not exists in the input StructuredRecord.");
-    } else {
-      LOG.info("Message is {}", Bytes.toString(message));
-      String multiMessageBody = new String(message, StandardCharsets.UTF_8);
-
-      int startIndex = 0;
-      int endIndex;
-      while (true) {
-        endIndex = multiMessageBody.indexOf("{\"table\":", startIndex + 5);
-        String jsonMessage = multiMessageBody.substring(startIndex);
-        if (endIndex != -1) {
-          jsonMessage = multiMessageBody.substring(startIndex, endIndex);
-        }
-        // This is the message you need process.
-        emitRecord(jsonMessage, emitter);
-        if (endIndex == -1) {
-          break;
-        }
-        multiMessageBody = multiMessageBody.substring(endIndex);
-        startIndex = 0;
-      }
+      throw new IllegalStateException(String.format("Input record does not contain the field '%s'.",
+                                                    config.getInputField()));
     }
+
+    String messageBody = new String(message, StandardCharsets.UTF_8);
+
+    LOG.info("XXX Received message body is {}", messageBody);
+    if (messageBody.contains("generic_wrapper") && messageBody.contains("oracle.goldengate")) {
+      // Current message is the schema of generic AVRO wrapper. Can be ignored.
+      /*
+        {
+          "type" : "record",
+          "name" : "generic_wrapper",
+          "namespace" : "oracle.goldengate",
+          "fields" : [ {
+            "name" : "table_name",
+            "type" : "string"
+          }, {
+            "name" : "schema_fingerprint",
+            "type" : "long"
+          }, {
+            "name" : "payload",
+            "type" : "bytes"
+          } ]
+        }
+       */
+      LOG.info("Ignoring the Generic Avro Wrapper schema.");
+      return;
+    }
+
+    if (messageBody.contains("\"type\" : \"record\"")) {
+      // Current message is the Schema of the table
+      // Create DDLRecord for it.
+      LOG.info("Emitting schema for the table {}", messageBody);
+      JsonObject schemaObj = GSON.fromJson(messageBody, JsonObject.class);
+      String namespaceName = schemaObj.get("namespace").getAsString();
+      String tableName = schemaObj.get("name").getAsString();
+
+      org.apache.avro.Schema avroSchema = new org.apache.avro.Schema.Parser().parse(messageBody);
+      long schemaFingerPrint = SchemaNormalization.parsingFingerprint64(avroSchema);
+      LOG.info("Populating Schema cache with namespace {}, tableName {}, and fingerPrint {}", namespaceName, tableName,
+               schemaFingerPrint);
+      // TODO Should we put namespace name here???
+      schemaCache.put(tableName, schemaFingerPrint, avroSchema);
+
+      StructuredRecord.Builder builder = StructuredRecord.builder(CDC_DDL_SCHEMA);
+      builder.set("schema", messageBody);
+      emitter.emit(builder.build());
+      return;
+    }
+
+    // Current message is the Wrapped Avro binary message
+    org.apache.avro.Schema avroGenericWrapperSchema = getGenericWrapperSchema();
+    GenericRecord genericRecord = getRecord(message, avroGenericWrapperSchema);
+    // TODO Generic Avro wrapped message does not have namespace name.
+    String tableName = (String) genericRecord.get("table_name");
+    long schameHashId = (Long) genericRecord.get("schema_fingerprint");
+    byte[] payload = (byte[]) genericRecord.get("payload");
+    LOG.info("Got tableName {} and fingerPrint {} in wrapped schema.", tableName, schameHashId);
+    org.apache.avro.Schema avroSchema = schemaCache.get(tableName, schameHashId);
+
+    StructuredRecord structuredRecord = AvroConverter.fromAvroRecord(getRecord(payload, avroSchema),
+                                                                     AvroConverter.fromAvroSchema(avroSchema));
+
+    LOG.info("Emitting Structured Record {}", structuredRecord);
+    emitter.emit(structuredRecord);
   }
 
-  private void emitRecord(String message, Emitter<StructuredRecord> emitter) {
-    JsonObject object = GSON.fromJson(message, JsonObject.class);
-    StructuredRecord.Builder builder = StructuredRecord.builder(CDC_SCHEMA);
-    builder.set("table", object.get("table").getAsString());
-    builder.set("op_type", object.get("op_type").getAsString());
-    builder.set("op_ts", object.get("op_ts").getAsString());
-    builder.set("current_ts", object.get("current_ts").getAsString());
-    LOG.info("Operation Type {}", object.get("op_type").getAsString().charAt(0));
-    switch (object.get("op_type").getAsString().charAt(0)) {
-      case 'I':
-        builder.set("after", getTableData("after", object));
-        builder.set("before", null);
-        break;
-      case 'U':
-        builder.set("before", getTableData("before", object));
-        builder.set("after", getTableData("after", object));
-        break;
-      case 'D':
-        builder.set("before", getTableData("before", object));
-        builder.set("after", null);
-        break;
-      default:
-        throw new IllegalArgumentException("invalid operation type encountered");
-    }
-    emitter.emit(builder.build());
+  private org.apache.avro.Schema getGenericWrapperSchema() {
+    String avroGenericWrapperSchema = "{\n" +
+      "          \"type\" : \"record\",\n" +
+      "          \"name\" : \"generic_wrapper\",\n" +
+      "          \"namespace\" : \"oracle.goldengate\",\n" +
+      "          \"fields\" : [ {\n" +
+      "            \"name\" : \"table_name\",\n" +
+      "            \"type\" : \"string\"\n" +
+      "          }, {\n" +
+      "            \"name\" : \"schema_fingerprint\",\n" +
+      "            \"type\" : \"long\"\n" +
+      "          }, {\n" +
+      "            \"name\" : \"payload\",\n" +
+      "            \"type\" : \"bytes\"\n" +
+      "          } ]\n" +
+      "        }";
+    return new org.apache.avro.Schema.Parser().parse(avroGenericWrapperSchema);
   }
 
-  private Map<String, String> getTableData(String name, JsonObject object) {
-    JsonObject after = object.getAsJsonObject(name);
-    Map<String, String> values = new HashMap<>();
-    for (Map.Entry<String, JsonElement> entry : after.entrySet()) {
-      values.put(entry.getKey(), entry.getValue().getAsString());
-    }
-    return values;
+  private GenericRecord getRecord(byte[] message, org.apache.avro.Schema schema) throws IOException {
+    GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+    InputStream is = new ByteArrayInputStream(message);
+    return datumReader.read(null, DecoderFactory.get().binaryDecoder(is, null));
   }
 
   public static class NormalizerConfig extends PluginConfig {
     @Name("inputField")
     @Description("Input field containing the payload of the message. Defaults to 'message'.")
     private String inputField;
+
+    @Name("schemaServiceName")
+    @Description("Name of the schema registry service. Name should be in the form of <application_name>:<service_name>")
+    private String schemaServiceName;
 
     public NormalizerConfig(String inputField) {
       this.inputField = inputField;
