d8ecb4b2c5b5caa92363e594d7585db8808e6163
sagarkapare <sagar@cask.co>
2017-05-24 14:09:07 -0700

    Added Golden Gate Kafka Source.

---
+++ cask-cdc/src/main/java/co/cask/cdc/plugins/common/SchemaUtils.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdc.plugins.common;
+
+import org.apache.avro.Schema;
+
+/**
+ * Utility class for Schema management.
+ */
+public final class SchemaUtils {
+
+  private SchemaUtils() {
+
+  }
+
+  public static Schema getGenericWrapperMessageSchema() {
+    String avroGenericWrapperSchema = "{\n" +
+      "          \"type\" : \"record\",\n" +
+      "          \"name\" : \"generic_wrapper\",\n" +
+      "          \"namespace\" : \"oracle.goldengate\",\n" +
+      "          \"fields\" : [ {\n" +
+      "            \"name\" : \"table_name\",\n" +
+      "            \"type\" : \"string\"\n" +
+      "          }, {\n" +
+      "            \"name\" : \"schema_fingerprint\",\n" +
+      "            \"type\" : \"long\"\n" +
+      "          }, {\n" +
+      "            \"name\" : \"payload\",\n" +
+      "            \"type\" : \"bytes\"\n" +
+      "          } ]\n" +
+      "        }";
+    return new Schema.Parser().parse(avroGenericWrapperSchema);
+  }
+}
